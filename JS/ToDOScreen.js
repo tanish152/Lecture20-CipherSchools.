5360a229bc2d671e77cc4aa1c59c73b1e863b192
tanish khokha <khokhatanish1@gmail.com>
2023-08-18 12:25:13 +0530

    lecture19-1-Cipherscholl

---
+++ JS/ToDOScreen.js
@@ -0,0 +1,19 @@
+ import { Component} from"react";
+ class ToDoScreen extends Component{
+    render
+    ()
+    return
+    {
+        {
+        
+        <div className="Screen">     
+        <h1 className="ui heading center"> To dOlist</h1>
+        </div>
+        <button className="ui secondary button"> Add task </button>
+        </div>
+        </div>
+
+    };
+ }
+}
+ export default ToDoScreen;
