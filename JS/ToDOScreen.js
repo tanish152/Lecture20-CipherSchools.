e6e1287f3ffa3149db3ffa4713997e4efa23d029
tanish khokha <khokhatanish1@gmail.com>
2023-08-18 12:32:16 +0530

    firstcommit

--- JS/ToDOScreen.js
+++ JS/ToDOScreen.js
@@ -3,10 +3,10 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';  
 export default function BasicButtons() {  
   return (  
-    <Stack spacing={2} direction="row">  
+    <Stack spacing={2} direction="row">   
    <Button variant="text">Text</Button>  
       <Button variant="contained">Contained</Button>  
-      <Button variant="outlined">Outlined</Button>  
+       <Button variant="outlined">Outlined</Button>  
     </Stack>  
   );  
 }  
